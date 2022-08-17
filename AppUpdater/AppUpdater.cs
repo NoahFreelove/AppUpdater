ae7dd31bb374960d3285c26e0cc0a63a1f395499
NoahFreelove <90488068+NoahFreelove@users.noreply.github.com>
2022-08-17 18:17:12 -0400

    Initial Commit

---
+++ AppUpdater/AppUpdater.cs
@@ -0,0 +1,29 @@
+﻿namespace AppUpdater;
+
+public static class AppUpdater
+{
+    public static bool hasInit = false;
+    
+    public static string appID;
+    public static string buildID;
+    public static string key; // Optional, only required if set in build settings
+    public static string branch;
+    
+    public static bool updateAvailable;
+    
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static void Init(string appID, string buildID, string key, string branch)
+    {
+        AppUpdater.appID = appID;
+        AppUpdater.buildID = buildID;
+        AppUpdater.key = key;
+        AppUpdater.branch = branch;
+        hasInit = true;
+    }
+
+    public static void Main()
+    {
+        Init("dfc9XpALPQ9XLlmIvMTU", "notBuildID", "thisisakey", "master");
+        Updater.CheckForUpdates();
+    }
+}
---
+++ AppUpdater/Updater.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+
+namespace AppUpdater;
+
+public static class Updater
+{
+    private static bool isUpdateReady;
+    private static bool downloadingUpdate;
+    private static float downloadProgress;
+    public static bool CheckForUpdates(bool updateIfAvailable = false)
+    {
+        AppUpdater.updateAvailable = IsUpdateAvailable();
+
+        if (updateIfAvailable && AppUpdater.updateAvailable)
+        {
+            StartUpdate();
+        }
+
+        return AppUpdater.updateAvailable;
+    }
+
+    public static void StartUpdate()
+    {
+        if(downloadingUpdate)
+            return;
+
+        if (isUpdateReady)
+        {
+            Update();
+            return;
+        }
+        DownloadUpdate();
+
+    }
+
+    private static void DownloadUpdate()
+    {
+        //
+    }
+
+    private static void Update()
+    {
+        
+    }
+
+    private static bool IsUpdateAvailable()
+    {
+        if(!AppUpdater.hasInit)
+            return false;
+
+        // Send GET request to the server
+        var url = "http://localhost:3000/activebuild/?appId=" + AppUpdater.appID + "&branch=" + AppUpdater.branch + "&key=" + AppUpdater.key;
+        
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri(url);
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        
+        HttpResponseMessage responseMessage = client.GetAsync(url).Result;
+        if (responseMessage.IsSuccessStatusCode)
+        { 
+            return responseMessage.Content.ReadAsStringAsync().Result != AppUpdater.buildID;
+        }
+        
+        return false;
+    }
+}
