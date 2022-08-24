158b8c2a994d59c444b771cb12b0968044dcd475
NoahFreelove <90488068+NoahFreelove@users.noreply.github.com>
2022-08-24 01:02:16 -0400

    Update Main.cs

--- TestProject/Main.cs
+++ TestProject/Main.cs
@@ -4,10 +4,19 @@ namespace TestProject;
 
 using static AppUpdater.AppUpdater;
 
-class Test
+static class Test
 {
     public static void Main(string[] args)
     {
+        if(!Downloader.IsApiAlive())
+        {
+            Console.WriteLine("Api Is Down");
+        }
+        else
+        {
+            Console.WriteLine("Api Is Running");
+        }
+
         Console.WriteLine("Welcome to the AppUpdater test program");
         string? input = Console.ReadLine();
         if(input != null && input.Contains("quit"))
@@ -15,11 +24,21 @@ class Test
             return;
         }
         
-        //Init();
+        /*Init(
+         "your app id",
+            "branch name to download from", 
+            "folder to download the update to. Typically your app's root folder. ABSOLUTE PATH REQUIRED", 
+            "path to your app's executable. Should probably be in the updateFolderPath", 
+            "the current build id. not required, but the updater will always think a update is available", 
+            "some app key. Not required if you disabled keys in your app updater settings"
+            );*/
+        
+        SetDownloadCompletedCallback(Console.WriteLine);
+        
         if (Updater.CheckForUpdates())
         {
             Console.WriteLine("Start Update");
-            Downloader.DownloadUpdate(true);
+            Downloader.DownloadUpdate(false);
         }
         Console.ReadKey();
     }
